4298c774c721dcd521e3fb5b295f548d789e7e65
MavisAffum <133819225+MavisAffum@users.noreply.github.com>
2023-09-10 14:03:20 -0500

    I updated url
    I changed URL from https://qa.koel.io to https://testpro.io

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,12 +17,11 @@ public class  LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.io/";
-        String url =  "https://testpro.io/";
+        String url = "https://testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
-        I'm Mavis
+
 
 
     }
