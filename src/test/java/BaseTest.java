ffa322f1cc40dd78a47227e993ea41ccbe3bb02a
MavisAffum <133819225+MavisAffum@users.noreply.github.com>
2023-09-18 12:58:46 -0500

    Created playSong
    Added playSong test and refactored helpers into BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,154 @@
+import com.sun.source.doctree.SeeTree;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.By
+
+import java.time.Duration;
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 public class BaseTest {
-
+ public WebDriver = "null";
+ public String url = "https://qa.koel.app";
+    public WebElement songsToPlaylists;
+  driver.get(url);
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+
+     @BeforeMethod
+      public void launchBrowser(){
+        //Added chromeOptions argument below to fix websocket error
+        //Added chromeOptions argument below to maximize screen
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("---start:maximize");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+     }
+    public void navigateToPage() {
+            String url = https://qa.koel.app;
+            driver.get(url);
+        }
+        public void provideEmail (String email){
+            webElement provideEmail = driver.findElement(By.CssSelector([type="email"]);
+            provideEmail.clear
+            provideEmail.sendKeys(---keysToSend:"email");
+        }
+
+           public void providePassword (String email){
+                webElement providePassword = driver.findElement(By.CssSelector([type="password"]);
+                providePassword.clear
+                providePassword.sendKeys(---keysToSend:"password");
+        }
+
+                public void clickSubmit() {
+                    webElement loginBtn = driver.findElement(By.ByCssSelector([type="submit"]);
+                    loginBtn.click;
+        }
+
+                    public void searchField(String randomGenerator) {
+                     webElement searchField = driver.findElement(By.CssSelector(input[type="search"]);
+                     Thread.sleep(millis:2000);
+        }
+                     public void selectFirstSong() {
+                            webElement selectFirstSong = driver.findElement(By.ByCssSelector([section="playlists"]);
+                            selectFirstSong.click;
+                            Thread.sleep(millis:2000);
+        }
+                                    public void clickToViewAllBtn() {
+                                webElement clickToViewAllBtn = driver.findElement(By.CssSelector(["data-test=\"view-all-songs-btn\""]);
+                                clickToViewAllBtn.click;
+                                Thread.sleep(millis:2000);
+        }
+                                public void clickAddToBtn() {
+            webElement clickAddToBtn = drive.findElement(By.CssSelector[data - test = "add-to-btn"]);
+            clickAddToBtn.click;
+            Thread.sleep(millis:2000);
+        }
+        public void choosePlaylist(){
+        webElement choosePlaylist = driver.findElement(By.ByCssSelector([""]);
+        choosePlaylist.click;
+        Thread.sleep(millis:2000);
     }
+<<<<<<< Updated upstream
+
+
+    @BeforeMethod
+    public void launchBrowser() {
+        public void addSongsToPlaylists () {
+            chromeOptions options = new chromeOptions();
+            optiens.addArguments("---remote-allow-origins");
+
+            webDriver driver = new chromeDriver(options);
+            driver.manage().timeouts.implicitlywaiit(Duration.ofSeconds(10));
+
+
+            public void navigateToPage(){
+
+            public void provideEmail(){
+                email.clear();
+                email.sendkeys(... keysToSend:demo@class.com);
+
+                public void providePassword(){
+                    password.clear();
+                    password.sendkeys(...keysToSend:te$t$tudent);
+
+                    public void clickSubmit(){
+                        Submit.click();
+
+                        public void searchField(String name); throw interruptedExpection {
+              webElement searchField = driver.findElement(By.cssSelector("div[id= 'searchForm']"));
+                searchfield.click();
+                searchfield.sendkeys(...keysToSend);
+                thread.sleep(millis:2000);
+
+            public void clickToViewAllBtn(); throw  interruptedExpection {
+                webElement clickToViewAllBtn = driver.findElement(By.xpath("button[data-test = 'view-all-songs-btn']"));
+                ToViewAllBtn.click();
+                thread.sleep(millis:2000);
+
+
+            public void selectFirstSongResult() throw interruptedExpection{
+            webElement FirstSong = driver.findElement(By.cssSelector("section[@id = 'songResultsWrapper']//tr[@class = 'song-item'][1]"));
+                firstSong.click;
+                thread.sleep(millis:2000);
+
+                public void clickAddToBtn() throw interruptedException{
+                    addToBtn.click();
+                    thread.sleep(millis:2000);
+
+                    public void choosePlaylist throw interruptedExpection(){
+                     webElement  playlist = driver.findElement(By"[]");
+                     playlist.click();
+                     thread.sleep(millis:2000);
+                                }
+                            public WebElement songsToPlaylists;
+                    driver.get(url);
+
+                Assert.assertTrue(driver.getNotificationText().contains(Homework17Created));
 }
+=======
+      webElement avatarIcon = drivr.findElement(By.ByCssSelector(div[id="searchForm"]);
+      Assert.assertTrue(avatarIcon.isDisplayed();
+    }
+      @AfterMethod
+    public void closeBrowser() {
+          driver.quit();
+      }
+
+
+>>>>>>> Stashed changes
